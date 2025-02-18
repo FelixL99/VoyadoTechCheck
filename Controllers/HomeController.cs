eb980b357f1e76185d069706597417f918ebcae0
FelixL99 <felixx.lindgren99@gmail.com>
2025-02-18 19:22:36 +0100

    Added SearchGoogle. Not connecting currently

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using VoyadoTechCheck.Models;
 
 namespace VoyadoTechCheck.Controllers;
@@ -7,10 +11,12 @@ namespace VoyadoTechCheck.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly HttpClient _httpClient;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _httpClient = new HttpClient();
     }
 
     [HttpGet]
@@ -28,8 +34,21 @@ public class HomeController : Controller
             return View(inputModel);
         }
 
-        ViewBag.Result = inputModel.UserInput;
+        ViewBag.Result = SearchGoogle(inputModel.UserInput); // inputModel.UserInput;
         return View(inputModel);
+    }
+
+    private async Task<long> SearchGoogle(string word)
+    {
+        string apiKey = "AIzaSyBS4B-68jVxnpB02oksdHopckI_wl8jPPU";
+        string cx = "a1b368a2a39734cba";
+        string url = $"https://www.googleapis.com/customsearch/v1?q={word}&key={apiKey}&cx={cx}";
+
+        var response = await _httpClient.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+        var json = JsonDocument.Parse(content);
+
+        return json.RootElement.GetProperty("searchInformation").GetProperty("totalResults").GetInt64();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Models/InputModel.cs
+++ Models/InputModel.cs
@@ -3,6 +3,5 @@ namespace VoyadoTechCheck.Models
     public class InputModel
     {
         public string UserInput { get; set; }
-        //public Dictionary<string, long> Results { get; set; } = new Dictionary<string, long>();
     }
 }
