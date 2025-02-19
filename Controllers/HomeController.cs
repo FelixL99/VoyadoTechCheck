86af9b2f8b9ff81fb0fe8041bc8475953d59dfbc
Felix Laptop <felixx.lindgren99@gmail.com>
2025-02-19 21:23:26 +0100

    Found new method for calling Google Search API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
 using VoyadoTechCheck.Models;
 
 // Algolia API
 using Algolia.Search.Clients;
 using Algolia.Search.Models.Search;
 
+// Google Custom Search API
+using Google.Apis.CustomSearchAPI.v1;
+using Google.Apis.Services;
+
 namespace VoyadoTechCheck.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly HttpClient _httpClient;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-        _httpClient = new HttpClient();
     }
 
     [HttpGet]
@@ -55,12 +56,12 @@ public class HomeController : Controller
     {
         // Setup Algolia API to get AppID and api key
         // https://dashboard.algolia.com/apps/1SQES8SXAC/dashboard
-        string ApplicationId = "1SQES8SXAC";
-        string apiKey = "1c3400b473280bce2cbf095fb170baa1";
+        const string ApplicationId = "1SQES8SXAC";
+        const string apiKey = "1c3400b473280bce2cbf095fb170baa1";
 
         // Index is loaded with the following dataset
         // https://www.kaggle.com/datasets/krishnanshverma/imdb-movies-dataset
-        string IndexName = "VoyadoTechCheck";
+        const string IndexName = "VoyadoTechCheck";
 
         // Setup Algolia SearchClient
         var client = new SearchClient(ApplicationId, apiKey);
@@ -74,28 +75,31 @@ public class HomeController : Controller
 
     // SearchGoogle - Calls Google Custom Search API and return parsed result
     // Doc: https://developers.google.com/custom-search/v1/overview
+    // https://developers.google.com/custom-search/v1/reference/rest
+    // https://blog.expertrec.com/google-custom-search-in-c/
     private async Task<long> SearchGoogle(string word)
     {
         // Setup Custom Search JSON API to get apiKey and cx and url
         // https://developers.google.com/custom-search/v1/overview
+        const string apiKey = "AIzaSyBS4B-68jVxnpB02oksdHopckI_wl8jPPU";
+        const string cx = "a1b368a2a39734cba";
 
-        string apiKey = "AIzaSyBS4B-68jVxnpB02oksdHopckI_wl8jPPU";
-        string cx = "a1b368a2a39734cba";
-        string url = $"https://www.googleapis.com/customsearch/v1?q={word}&key={apiKey}&cx={cx}";
+        // Setup custom search service
+        var customSearch = new CustomSearchAPIService(new BaseClientService.Initializer { 
+            ApiKey = apiKey, 
+            ApplicationName = cx 
+        });
 
-        // Call API and get search content
-        var response = await _httpClient.GetAsync(url);
-        var content = await response.Content.ReadAsStringAsync();
-        
-        // Parse JSON result file
-        var json = JsonDocument.Parse(content);
-        var resultString = json.RootElement
-            .GetProperty("searchInformation")
-            .GetProperty("totalResults")
-            .GetString();
+        // Perform search
+        var listRequest = customSearch.Cse.List();
+        listRequest.Cx = cx;
+        listRequest.Q = word; // Query
 
-        // Return result if valid, otherwise return 0
-        return long.TryParse(resultString, out var result) ? result : 0;
+        // Execute search
+        var search = await listRequest.ExecuteAsync();
+
+        // Return result if parse works, otherwise return 0
+        return long.TryParse(search.SearchInformation.TotalResults, out var result) ? result : 0; 
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
