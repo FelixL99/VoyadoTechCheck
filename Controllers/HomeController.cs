fb92ea87f8acc3164a3593129dd1135379aa2476
Felix Laptop <felixx.lindgren99@gmail.com>
2025-02-23 13:51:23 +0100

    Removed some comments and updated UserInput error handling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,43 +33,36 @@ public class HomeController : Controller
         // Check if user input is valid
         if (string.IsNullOrWhiteSpace(searchModel.UserInput))
         {
-            ModelState.AddModelError("UserInput", "Please enter valid input.");
+            // UserInput not valid
             return View(searchModel);
         }
 
-        // Split input
         string[] words = searchModel.UserInput.Split(' ');
 
-        // Call SearchGoogle for each word
         foreach (string word in words)
         {
             searchModel.TotalGoogleSearchHits += await SearchGoogle(word);
             searchModel.TotalAlgoliaSearchHits += await SearchAlgolia(word);
         }
-
         return View(searchModel);
     }
 
     // SearchAlgolia - Calls Algolia Search API and return number of hits
     // Doc: https://www.algolia.com/doc/libraries/csharp/v7/
+    // Setup Algolia API to get AppID and api key
+    // https://dashboard.algolia.com/apps/1SQES8SXAC/dashboard
+    // Index is loaded with the following dataset
+    // https://www.kaggle.com/datasets/krishnanshverma/imdb-movies-dataset
     private async Task<long> SearchAlgolia(string word)
     {
-        // Setup Algolia API to get AppID and api key
-        // https://dashboard.algolia.com/apps/1SQES8SXAC/dashboard
         const string ApplicationId = "1SQES8SXAC";
         const string apiKey = "1c3400b473280bce2cbf095fb170baa1";
-
-        // Index is loaded with the following dataset
-        // https://www.kaggle.com/datasets/krishnanshverma/imdb-movies-dataset
         const string IndexName = "VoyadoTechCheck";
 
-        // Setup Algolia SearchClient
         var client = new SearchClient(ApplicationId, apiKey);
+        var searchResults = await client.SearchSingleIndexAsync<Hit>(IndexName, 
+            new SearchParams(new SearchParamsObject { Query = word }));
 
-        // Search for word in with Algolia Search API
-        var searchResults = client.SearchSingleIndex<Hit>(IndexName, new SearchParams(new SearchParamsObject { Query = word }));
-
-        // Return search hits
         return searchResults.Hits.Count;
     }
 
@@ -77,28 +70,24 @@ public class HomeController : Controller
     // Doc: https://developers.google.com/custom-search/v1/overview
     // https://developers.google.com/custom-search/v1/reference/rest
     // https://blog.expertrec.com/google-custom-search-in-c/
+    // Setup Custom Search JSON API to get apiKey and cx and url
+    // https://developers.google.com/custom-search/v1/overview
     private async Task<long> SearchGoogle(string word)
     {
-        // Setup Custom Search JSON API to get apiKey and cx and url
-        // https://developers.google.com/custom-search/v1/overview
         const string apiKey = "AIzaSyBS4B-68jVxnpB02oksdHopckI_wl8jPPU";
-        const string cx = "a1b368a2a39734cba";
+        const string ApplicationId = "a1b368a2a39734cba";
 
-        // Setup custom search service
         var customSearch = new CustomSearchAPIService(new BaseClientService.Initializer { 
             ApiKey = apiKey, 
-            ApplicationName = cx 
+            ApplicationName = ApplicationId
         });
 
-        // Perform search
         var listRequest = customSearch.Cse.List();
-        listRequest.Cx = cx;
+        listRequest.Cx = ApplicationId;
         listRequest.Q = word; // Query
 
-        // Execute search
         var search = await listRequest.ExecuteAsync();
 
-        // Return result if parse works, otherwise return 0
         return long.TryParse(search.SearchInformation.TotalResults, out var result) ? result : 0; 
     }
 
--- Models/SearchModel.cs
+++ Models/SearchModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VoyadoTechCheck.Models
 {
     public class SearchModel
     {
+        [Required(ErrorMessage = "Please enter valid input.")]
         public string UserInput { get; set; } = "";
 
         public long TotalGoogleSearchHits { get; set; } = 0;
